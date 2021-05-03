63e657116d6e92f80fe2c4ec554f52ae12f03a4f
Péricles A. N. Tavares <pericles.eletronica@gmail.com>
2021-05-03 13:01:58 -0300

    adicionando arquivos

---
+++ ASP.NET_MONGO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ASP.NET_MONGO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
